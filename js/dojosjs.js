ed04e126c2a58276991715675845fdf680084ff7
NabilAch <nab.aourach@gmail.com>
2021-01-22 12:05:31 +0100

    Eval JS

--- js/dojosjs.js
+++ js/dojosjs.js
@@ -1,1 +1,15 @@
 console.log("Go Chuck !");
+
+
+let okChuck = document.querySelector('main > section > aside > div');
+
+okChuck.addEventListener('mousemove', function () {
+
+    okChuck.style.borderRadius = "0%";
+
+});
+
+okChuck.addEventListener('mouseleave', function () {
+    okChuck.style.borderRadius = "50%";
+});
+
